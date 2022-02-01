2adc4b4b9f9291763ec42f70c626ebd5fb998546
arsalan rahimi <years1373@yahoo.com>
2022-02-01 02:48:46 -0800

    item_router

---
+++ api/item/item_router.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const router = express.Router()
+// const Item = require("./item-model.js")
+// const {} = require("./item-middleware")
+
+module.exports = router
